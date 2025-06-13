e61cc8e1b8341532947993c86933e56ce9d8a59d
k4m4 <nikolaskam@gmail.com>
2025-06-13 13:13:13 +0300

    chore(xrpl-voting-verifier): bump BASE_VERSION to v1.3.0

--- contracts/xrpl-voting-verifier/src/contract.rs
+++ contracts/xrpl-voting-verifier/src/contract.rs
@@ -14,7 +14,7 @@ mod query;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-const BASE_VERSION: &str = "1.2.0";
+const BASE_VERSION: &str = "1.3.0";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
